c8dafefaef731f97fc60c9677af6e06764eb3216
GrigoryCheremin <97213552+GrigoryCheremin@users.noreply.github.com>
2022-08-18 20:30:32 +0300

    Update staya_autorisation.js

--- 1-My-tests/staya_autorisation.js
+++ 1-My-tests/staya_autorisation.js
@@ -3,8 +3,8 @@ describe('Тестирование авторизации в Staya', function ()
    it('Проверка авторизации верные данные', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('grixa83liv05@yandex.ru');
-        cy.get('.auth-form > form > [type="password"]').type('chge23051983chge');
+        cy.get('.auth-form > form > [type="email"]').type('Ввести зарегистрированный e-mail');
+        cy.get('.auth-form > form > [type="password"]').type('Ввести пароль, соответствующий зарегистрированному e-mail');
         cy.get('.auth-form__submit').click();
     })
 
@@ -18,8 +18,8 @@ describe('Тестирование авторизации в Staya', function ()
    it('Проверка авторизации неверный пароль', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('grixa83liv05@yandex.ru');
-        cy.get('.auth-form > form > [type="password"]').type('chge23051983chg');
+        cy.get('.auth-form > form > [type="email"]').type('Ввести зарегистрированный e-mail');
+        cy.get('.auth-form > form > [type="password"]').type('Ввести пароль, не соответствующий зарегистрированному e-mail');
         cy.get('.auth-form__submit').click();
         cy.contains('Невозможно войти с предоставленными учетными данными.');
     })
